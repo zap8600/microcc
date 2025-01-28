84e730d9d0ed11000a20524d67bba01653ee810f
zap8600 <95487746+zap8600@users.noreply.github.com>
2025-01-28 09:51:42 -0600

    eh

--- main.c
+++ main.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 
-char *pos; // Pointer to current position in code
-int token; // Current token
-
 void next() {
-    while(token = *pos) {
-        pos++;
-        
-        if(token == '#') {
-            // Skip includes and defines
-            while(*pos != 0 && *p != '\n') pos++;
-        }
-    }
+    //
 }
 
 int main(int argc, char *argv[]) {
